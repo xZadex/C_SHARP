ec8dc7cd17b1a4c000f589fa50648d7434a98e33
Nicholas <codebyzade@gmail.com>
2022-11-28 17:28:42 -0500

    Debugging practice completed

---
+++ Week_1/Debugging_Practice/Program.cs
@@ -0,0 +1,52 @@
+﻿// Challenge 1
+// bool amProgrammer = true;
+// int Age = 27;
+// List<string> Names = new List<string>();
+// Names.Add("Monica");
+// Dictionary<string, int> MyDictionary = new Dictionary<string, int>();
+// MyDictionary.Add("Hello", 0);
+// MyDictionary.Add("Hi there", 0);
+// // This is a tricky one! Hint: look up what a char is in C#
+// string MyName = "MyName";
+
+// Challenge 2
+// List<int> Numbers = new List<int>() {2,3,6,7,1,5};
+// for(int i = 0; i < Numbers.Count; i++)
+// {
+//     Console.WriteLine(Numbers[i]);
+// }
+
+// Challenge 3
+// List<int> MoreNumbers = new List<int>() {12,7,10,-3,9};
+// foreach(int i in MoreNumbers)
+// {
+//     Console.WriteLine(i);
+// }
+
+// Challenge 4
+// List<int> EvenMoreNumbers = new List<int> {3,6,9,12,14};
+// foreach(int num in EvenMoreNumbers)
+// {
+//     if(num % 3 == 0)
+//     {
+//         num = 0;
+//     }
+// }
+// Can't assign the list you are looping through?
+
+
+// Challenge 5
+// What can we learn from this error message?
+// string MyString = "superduberawesome";
+// MyString[7] =  'p';
+// can't be reassigned?
+
+// Challenge 6
+// Hint: some bugs don't come with error messages
+// Random rand = new Random();
+// int randomNum = rand.Next(13);
+// Console.WriteLine(randomNum);
+// if(randomNum == 12)
+// {
+//     Console.WriteLine("Hello");
+// }
