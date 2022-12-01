b21c483959f809c4fd8bb1ba98edf97ed00763be
Nicholas <codebyzade@gmail.com>
2022-12-01 02:44:49 -0500

    count down assignment completed

---
+++ Week_1/Countdown/Controllers/CountdownController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Countdown.Controllers;
+public class CountdownController : Controller
+{
+
+    [HttpGet("")]
+    public ViewResult Index()
+    {
+        return View();
+    }
+}
